582e57bda0198b7e6c08770bb1334c361d2eaccf
Mlothekile <thapelomlotha@gmail.com>
2023-06-29 17:09:01 +0200

    adding page icon

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>SHOP101</title>
+    <link rel="icon" href="assets/images/shop_icon.png" type="image/x-icon">
 
     <!--JAVA_SCRIPT-->
     <script src="https://kit.fontawesome.com/a035f24907.js" crossorigin="anonymous"></script>
